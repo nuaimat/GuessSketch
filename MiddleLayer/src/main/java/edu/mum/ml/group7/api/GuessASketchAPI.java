cc8cdb79312486f53c7aebe36fbfb5441d507e89
subhechha <subhechha@users.noreply.github.com>
2017-03-18 19:40:16 -0700

    add first guessimage api

--- MiddleLayer/src/main/java/edu/mum/ml/group7/api/GuessASketchAPI.java
+++ MiddleLayer/src/main/java/edu/mum/ml/group7/api/GuessASketchAPI.java
@@ -5,15 +5,26 @@ import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import static org.apache.http.HttpHeaders.USER_AGENT;
 
 
 /**
@@ -51,16 +62,74 @@ public class GuessASketchAPI {
     public Response guessImage(
             @FormDataParam("file") InputStream fileInputStream,
             @FormDataParam("label") String label
-            ) throws IOException {
-        String result = String.format("Image guessed image label (%s)", label) ;
+    ) throws IOException {
+        String url = "http://mo-macbook.local/app.py";
+        //String uploadedFileLocation = "D:/MachineLearning/uploads/" + "guess-" + java.util.UUID.randomUUID() + ".jpg";
+        File temp = File.createTempFile("guess-",".jpg", new File("D:/MachineLearning/uploads/"));
+        String uploadedFileLocation = temp.getName();
+        // save it
+        writeToFile(fileInputStream, temp);
 
-        /*final File tempFile = File.createTempFile("guess-a-sketch", "");
-        //tempFile.deleteOnExit();
-        try (FileOutputStream out = new FileOutputStream(tempFile)) {
-            IOUtils.copy(fileInputStream, out);
+        String result = doPost(url, temp, label);
+        temp.delete();
+
+        return Response.status(201).entity(result).build();
+    }
+
+    private String doPost(String url, File uploadedFile, String label) throws IOException {
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(url);
+        FileBody bin = new FileBody(uploadedFile);
+        StringBody id = new StringBody(label);
+
+        // add header
+        post.setHeader("User-Agent", USER_AGENT);
+        MultipartEntity reqEntity = new MultipartEntity();
+        reqEntity.addPart("file", bin);
+        reqEntity.addPart("label", id);
+
+
+        post.setEntity(reqEntity);
+
+        HttpResponse response = client.execute(post);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + post.getEntity());
+        System.out.println("Response Code : " +
+                response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result1 = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result1.append(line);
         }
 
-        result += "on " + tempFile.getAbsolutePath();*/
-        return Response.status(201).entity(result).build();
+        System.out.println(result1.toString());
+
+        return result1.toString();
+
+    }
+
+    private void writeToFile(InputStream uploadedInputStream,
+                             File tempFile) {
+
+        try {
+            OutputStream out = new FileOutputStream(tempFile);
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
